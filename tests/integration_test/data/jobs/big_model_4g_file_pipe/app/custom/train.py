883dcfddaaf9954e0cad246dac384692b9ddcc27
YuanTingHsieh <yuantingh@nvidia.com>
2026-03-18 01:32:44 +0000

    add more test jobs

---
+++ tests/integration_test/data/jobs/big_model_4g_file_pipe/app/custom/train.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import random
+import re
+import time
+from datetime import datetime
+
+import torch
+
+import nvflare.client as flare
+
+
+def evaluate(data):
+
+    t = time.time() / 1e10
+    print(f"fake evaluate data: {data}")
+    print(
+        f"fake evaluate result: {t}, generated at {datetime.utcfromtimestamp(t * 1e10).strftime('%Y-%m-%d %H:%M:%S')}"
+    )
+    return t
+
+
+def main():
+    flare.init()
+    input_model = flare.receive()
+
+    print("@@@ input_model: ", input_model)
+    round_num = input_model.current_round
+    print("@@@ Round number in this round: ", round_num)
+
+    site_name = input_model.meta.get("site_name")
+    multiplier = re.search(r"\d+", site_name).group()
+    print("@@@ site_name: ", site_name)
+
+    start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if input_model.current_round == 0:
+        weight = torch.zeros([10**5, 10**3], dtype=torch.float32)
+        bias = torch.zeros([10**5], dtype=torch.float32)
+    else:
+        weight = input_model.params.get("fc1.weight")
+        bias = input_model.params.get("fc1.bias")
+
+    zzz = random.uniform(1.0, 3.0)
+    print("@@@ Sleep " + str(zzz))
+    time.sleep(zzz)
+
+    weight = torch.add(weight, 1) * int(multiplier)
+    bias = torch.add(bias, 1) * int(multiplier)
+
+    end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info("Finished Training")
+
+    params = {"fc1.weight": weight, "fc1.bias": bias}
+
+    accuracy = evaluate(params)
+
+    output_model = flare.FLModel(
+        params=params,
+        metrics={"accuracy": accuracy},
+        meta={"NUM_STEPS_CURRENT_ROUND": 2, "start": start_time, "end": end_time},
+    )
+
+    print("@@@ output_model: ", output_model)
+    flare.send(output_model)
+
+
+if __name__ == "__main__":
+    main()
