20b31e8722301fa876c7b842fef1fc1713045bdc
YuanTingHsieh <yuantingh@nvidia.com>
2026-04-03 16:14:37 -0700

    fix

--- nvflare/lighter/impl/docker_launcher.py
+++ nvflare/lighter/impl/docker_launcher.py
@@ -92,6 +92,7 @@ class DockerLauncherBuilder(Builder):
                 replacement={
                     "admin_port": admin_port,
                     "fed_learn_port": fed_learn_port,
+                    "communication_port": lh.port,
                     "docker_image": self.docker_image,
                 },
                 exe=True,
